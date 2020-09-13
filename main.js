48d61e711e54ab16cced10f35b4df469d81d1635
Jegede Olatide <jegedeolatide@gmail.com>
2020-09-13 17:00:23 +0100

    edit hamburger menu

--- main.js
+++
@@ -1,6 +0,0 @@
-const icon = document.getElementById('icon');
-const myLinks = document.getElementById('myLinks');
-
-icon.addEventListener('click', () => {
-  myLinks.classList.toggle('show');
-});
